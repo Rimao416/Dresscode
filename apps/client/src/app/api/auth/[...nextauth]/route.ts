118b36a14902bd966e570f1682903a84d35e51b0
Rimao416 <kayumbaomari27@gmail.com>
2025-07-23 18:50:11 +0200

    :tada: Suprression de la partie client

--- apps/client/src/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,6 +0,0 @@
-// app/api/auth/[...nextauth]/route.ts
-import authConfig from '@/auth.config'
-import NextAuth from 'next-auth'
-const handler = NextAuth(authConfig)
-
-export { handler as GET, handler as POST }
